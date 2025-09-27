1d2dc784105dcd26a9e2e84cdc9a4cab46ddf62e
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-09-27 23:11:20 +0100

    adding tests

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -3,9 +3,27 @@ import {PgListenConnection} from '../src';
 
 const {pool} = initDb();
 
-describe('listen', () => {
-    it('must work', async () => {
+describe('constructor', () => {
+    it('must initialize correctly', async () => {
         const ls = new PgListenConnection({pool});
         expect(ls.isLive).toBe(true);
+        expect(ls.isConnected).toBeFalsy();
+        expect(ls.liveChannels).toEqual([]);
+    });
+});
+
+describe('listen', () => {
+    it('must loop events and data correctly', async () => {
+        const ls = new PgListenConnection({pool});
+        const onReady = jest.fn(async () => {
+            await ls.notify(['channel_1'], 'hello');
+        });
+        const obs = ls.listen(['channel_1'], onReady);
+        const sub = obs.subscribe(msg => {
+            expect(onReady).toHaveBeenCalledTimes(1);
+            expect(ls.liveChannels).toEqual(['channel_1']);
+            expect(msg.payload).toEqual('hello');
+            sub.unsubscribe();
+        });
     });
 });
